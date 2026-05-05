1be3a3e6961b87ef335e888e9c43628a3feb93fd
Guilherme Gonçalves <guilherme.g@ambevtech.com.br>
2026-05-05 19:49:15 -0300

    Revert "Merge branch 'main' of https://github.com/gsag/csharp-transactional-pattern"
    This reverts commit 7103dad82fdeaf0f5b8117633eebcb49bd4d8141, reversing
    changes made to 07b4615a51bd5da49a788cfe68e6c3063728f039.

--- src/Transactional.Core/Observability/NullTransactionObserver.cs
+++ src/Transactional.Core/Observability/NullTransactionObserver.cs
@@ -3,9 +3,9 @@ using Transactional.Core.Attributes;
 
 namespace Transactional.Core.Observability;
 
-public sealed class NullTransactionObserver : ITransactionLifecycleObserver
+internal sealed class NullTransactionObserver : ITransactionLifecycleObserver
 {
-    public static readonly NullTransactionObserver Instance = new();
+    internal static readonly NullTransactionObserver Instance = new();
 
     public void OnBegin(MethodInfo method, TransactionalAttribute attr) { }
     public void OnCommit(MethodInfo method, TimeSpan elapsed) { }
